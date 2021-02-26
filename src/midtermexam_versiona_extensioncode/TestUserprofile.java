3a050e55df85b6db80b526c163013c50e38688b3
leo9672 <chenyuk@sheridancollege.ca>
2021-02-26 10:25:02 -0500

    Update Card.java

---
+++ src/midtermexam_versiona_extensioncode/TestUserprofile.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package midtermexam_versiona_extensioncode;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author leo
+ */
+public class TestUserprofile {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        
+        System.out.println("Please enter your name: ");
+        String n = input.nextLine();
+        
+        System.out.println("Choose your favourite Genre: " );
+        System.out.println("Comedy, Drama, Drama, Mystery");  
+        String g = input.nextLine();
+        
+        System.out.println("Your userProfile was created.");
+    }
+}
+
--- src/midtermexam_versiona_extensioncode/UserProfile.java
+++ src/midtermexam_versiona_extensioncode/UserProfile.java
@@ -18,7 +18,7 @@ public class UserProfile
     
     private String userID;//the userID
     private String genre;// the user's preferred genre of movie
-    private String[] genres = {"Comedy", "Drama", "Action", "Mystery"};//available genres of movies
+    private String[] genres = {"Comedy", "Drama", "Drama", "Mystery"};//available genres of movies
     
     /**
      * A constructor that takes in the userID and the favourite genre
